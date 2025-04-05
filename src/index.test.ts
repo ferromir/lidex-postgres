5c1bf26c04d985757da0dca5024887d19b6b034d
Fernando Romero <fernando.romero@road.io>
2025-04-05 15:49:10 +0200

    checkpoint

--- src/index.test.ts
+++ src/index.test.ts
@@ -4,40 +4,108 @@ import {
 } from "@testcontainers/postgresql";
 import { makePostgresPersistence, Persistence } from ".";
 
+import pg from "pg";
+
 let container: StartedPostgreSqlContainer;
 let persistence: Persistence;
+let client: pg.Client;
 
-beforeEach(async () => {
+beforeAll(async () => {
   container = await new PostgreSqlContainer().start();
   const uri = container.getConnectionUri();
   persistence = await makePostgresPersistence(uri);
+  client = new pg.Client({ connectionString: uri });
+  await client.connect();
 }, 60_000);
 
-afterEach(async () => {
+afterAll(async () => {
+  await client.end();
   await persistence.terminate();
   await container.stop();
 });
 
+beforeEach(async () => {
+  await client.query("DELETE FROM workflows");
+  await client.query("DELETE FROM steps");
+  await client.query("DELETE FROM naps");
+});
+
 describe("insert", () => {
-  it("returns true if workflow is inserted", async () => {
-    const result = await persistence.insert(
-      "workflow-1",
-      "handler-1",
-      "input-1"
-    );
-
-    expect(result).toBeTruthy();
-  });
-
-  it("returns false if workflow already exists", async () => {
+  it("inserts a workflow", async () => {
     await persistence.insert("workflow-1", "handler-1", "input-1");
 
-    const result = await persistence.insert(
+    const result = await client.query(
+      "SELECT * FROM workflows WHERE id='workflow-1'"
+    );
+
+    expect(result.rows[0]).toEqual({
+      id: "workflow-1",
+      handler: "handler-1",
+      input: '"input-1"',
+      status: "idle",
+      timeout_at: null,
+      failures: null,
+      last_error: null,
+    });
+  });
+
+  it("is idempotent", async () => {
+    const result1 = await persistence.insert(
       "workflow-1",
       "handler-1",
       "input-1"
     );
 
-    expect(result).toBeFalsy();
+    const result2 = await persistence.insert(
+      "workflow-1",
+      "handler-1",
+      "input-1"
+    );
+
+    expect(result1).toBeTruthy();
+    expect(result2).toBeFalsy();
+  });
+});
+
+describe("claim", () => {
+  it("returns undefined if no workflow is claimed", async () => {
+    const workflowId = await persistence.claim(new Date(), new Date());
+    expect(workflowId).toBeUndefined();
+  });
+
+  it("claims idle workflows", async () => {
+    await client.query(`
+      INSERT INTO workflows (id, handler, input, status)
+      VALUES ('workflow-1', 'handler-1', '"input-1"', 'idle')
+    `);
+
+    const now = new Date();
+    const timeoutAt = new Date(now.getTime() + 1000);
+    const workflowId = await persistence.claim(now, timeoutAt);
+    expect(workflowId).toEqual("workflow-1");
+
+    const result = await client.query(
+      "SELECT * FROM workflows WHERE id='workflow-1'"
+    );
+
+    expect(result.rows[0]).toEqual({
+      id: "workflow-1",
+      handler: "handler-1",
+      input: '"input-1"',
+      status: "running",
+      timeout_at: timeoutAt,
+      failures: null,
+      last_error: null,
+    });
+  });
+});
+
+describe("findOutput", () => {
+  it("promise test", async () => {
+    const p = new Promise((resolve, reject) => {
+      reject(new Error("aborted"));
+      console.log("PRINTED ANYWAY!!");
+    });
+    await expect(p).rejects.toThrow("aborted");
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -28,6 +28,8 @@ type RunData = Pick<Workflow, "handler" | "input" | "failures">;
 
 export interface Persistence {
   insert(workflowId: string, handler: string, input: unknown): Promise<boolean>;
+  claim(now: Date, timeoutAt: Date): Promise<string | undefined>;
+  findOutput(workflowId: string, stepId: string): Promise<unknown>;
   terminate(): Promise<void>;
 }
 
@@ -39,28 +41,28 @@ export async function makePostgresPersistence(
   await client.connect();
 
   await client.query(`
-    CREATE TABLE IF NOT EXISTS lidexworkflows (
+    CREATE TABLE IF NOT EXISTS workflows (
       id TEXT PRIMARY KEY,
       handler TEXT NOT NULL,
       input TEXT NOT NULL,
-      "status" TEXT NOT NULL,
-      timeoutAt TIMESTAMP WITH TIME ZONE,
+      status TEXT NOT NULL,
+      timeout_at TIMESTAMP WITH TIME ZONE,
       failures INTEGER,
-      lastError TEXT
+      last_error TEXT
     );
   `);
 
   await client.query(`
-    CREATE TABLE IF NOT EXISTS lidexworkflowsteps (
+    CREATE TABLE IF NOT EXISTS steps (
       id TEXT PRIMARY KEY,
-      "output" TEXT NOT NULL
+      output TEXT NOT NULL
     );
   `);
 
   await client.query(`
-    CREATE TABLE IF NOT EXISTS lidexworkflownaps (
+    CREATE TABLE IF NOT EXISTS naps (
       id TEXT PRIMARY KEY,
-      wakeUpAt TIMESTAMP WITH TIME ZONE NOT NULL
+      wake_up_at TIMESTAMP WITH TIME ZONE NOT NULL
     );
   `);
 
@@ -72,7 +74,7 @@ export async function makePostgresPersistence(
     const inputJson = JSON.stringify(input);
     const result = await client.query(
       `
-      INSERT INTO lidexworkflows (id, handler, input, "status")
+      INSERT INTO workflows (id, handler, input, status)
       VALUES ($1, $2, $3, $4) ON CONFLICT (id) DO NOTHING;
       `,
       [workflowId, handler, inputJson, "idle"]
@@ -81,12 +83,60 @@ export async function makePostgresPersistence(
     return (result.rowCount || 0) > 0;
   }
 
+  async function claim(
+    now: Date,
+    timeoutAt: Date
+  ): Promise<string | undefined> {
+    const result = await client.query(
+      `
+      UPDATE workflows SET
+        "status" = 'running',
+        timeout_at = $2
+      WHERE id IN (
+        SELECT id FROM workflows 
+        WHERE
+          "status" = 'idle' OR
+          ("status" IN ('running', 'failed') AND timeout_at < $1)
+        LIMIT 1
+        FOR UPDATE SKIP LOCKED
+      )
+      RETURNING *
+    `,
+      [now, timeoutAt]
+    );
+
+    if (result.rowCount && result.rowCount > 0 && result.rows[0].id) {
+      return result.rows[0].id;
+    }
+
+    return undefined;
+  }
+
+  async function findOutput(
+    workflowId: string,
+    stepId: string
+  ): Promise<unknown> {
+    const id = `${workflowId}/${stepId}`;
+
+    const result = await client.query("SELECT * FROM steps WHERE id = $1", [
+      id,
+    ]);
+
+    if (result.rows[0]) {
+      return JSON.parse(result.rows[0].output);
+    }
+
+    return undefined;
+  }
+
   async function terminate() {
     await client.end();
   }
 
   return {
     insert,
+    claim,
+    findOutput,
     terminate,
   };
 }
